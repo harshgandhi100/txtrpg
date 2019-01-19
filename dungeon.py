7d6452b3ae13ea08f69327e40945025a0e6a568a
harshgandhi100 <harshgandhi100@gmail.com>
2019-01-19 17:50:53 +0530

    Changing Dungeon

--- dungeon.py
+++ dungeon.py
@@ -1,24 +1,12 @@
 import random
 import math
 
-class Adjnode:
-    def __init__(self,data):
-        self.roomId = data
-        self.link = None
-
-class Dungeon:
-    def __init__(self,n_rooms):
-        self.numberOfRooms = n_rooms
-        self.layout = [None] * numberOfRooms
-
-    def add_doors_between(self,source,destination):
-        connection = Adjnode(destination)
-        connection.link = self.layout[source]
-        self.layout[source] = connection
-
-        connection = Adjnode(source)
-        connection.link = self.layout[destination]
-        self.layout[destination] = connection
-
-    def next_room()
+class room:
+    room_count = 0
+    def __init__(self):
+        self.index = room_count
+        self.links = [None] * 4
     
+class dungeon:
+    def __init__(self,n_rooms):
+        
